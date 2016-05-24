4dcc998fceac3059467b87b95e08c1bef31c96f1
michi_sep <michael.gaensluckner@gmail.com>
2016-05-24 10:50:42 +0200

    RestaurantView

---
+++ ErasmusTU/RestaurantView.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantView.swift
+//  ErasmusTU
+//
+//  Created by Michael Gänsluckner on 24.05.16.
+//  Copyright © 2016 Michael Gänsluckner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RestaurantView: UIViewController {
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelSubtitle: UILabel!
+    @IBOutlet weak var labelAdress: UILabel!
+    @IBOutlet weak var labelLink: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        //self.labelTitle.text = "asd"
+        
+    }
+    
+}

cf4c2409824c596a4be8f7ab4a3d4c276e99dcff
michi_sep <michael.gaensluckner@gmail.com>
2016-06-01 14:21:19 +0200

    sport

---
+++ ErasmusTU/SportDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SportDetailViewController.swift
+//  ErasmusTU
+//
+//  Created by Michael Gänsluckner on 01.06.16.
+//  Copyright © 2016 Michael Gänsluckner. All rights reserved.
+//
+
+import UIKit
+var passedSport = ""
+
+class SportDetailViewController: UIViewController {
+    @IBOutlet var descrLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var linkButton: UIButton!
+    
+    
+    @IBAction func linkPressed(sender: AnyObject) {
+        
+    }
+    
+    override func viewDidLoad() {
+        
+    }
+}
+

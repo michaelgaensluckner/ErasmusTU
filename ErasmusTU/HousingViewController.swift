f549797d1705d7e2f2ebb860daeb057353adaf1c
Julia Martinez Ferrer <juliamartinezferrer@julias-mac.local>
2016-06-03 15:44:43 +0200

    Useful 1.0

---
+++ ErasmusTU/HousingViewController.swift
@@ -0,0 +1,87 @@
+//
+//  HousingViewController.swift
+//  ErasmusTU
+//
+//  Created by Julia Martinez Ferrer on 01/06/16.
+//  Copyright © 2016 Michael Gänsluckner. All rights reserved.
+//
+
+import UIKit
+
+class HousingViewController: UIViewController {
+var  dataToTransfer: String = ""
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button4: UIButton!
+    
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label3: UILabel!
+    @IBOutlet var label4: UILabel!
+    
+    
+    @IBAction func TuInfo(sender: UIButton) {
+       
+    dataToTransfer="http://portal.tugraz.at/portal/page/portal/Internationale_Beziehungen/Incomings/IncomingExchangeStudents/Vorbereitung"
+    self.performSegueWithIdentifier("HousingToInfo", sender: self)
+     }
+    
+    @IBAction func EsnInfo(sender: UIButton) {
+        dataToTransfer="http://unigraz.esnaustria.org/living-graz"
+        self.performSegueWithIdentifier("HousingToInfo", sender: self)
+    }
+    @IBAction func Oaed(sender: UIButton) {
+        dataToTransfer="http://housing.oead.at/de/unterkuenfte/graz-de/haeuser-de?view=application&city=graz"
+        self.performSegueWithIdentifier("HousingToInfo", sender: self)
+        
+    }
+    
+    @IBAction func Wist(sender: UIButton) {
+        dataToTransfer="http://www.wist.vc-graz.ac.at/wordpress/"
+        self.performSegueWithIdentifier("HousingToInfo", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        label1.layer.cornerRadius = 5
+        label2.layer.cornerRadius = 5
+        label3.layer.cornerRadius = 5
+        label4.layer.cornerRadius = 5
+        
+        label1.layer.masksToBounds=true
+        label2.layer.masksToBounds=true
+        label3.layer.masksToBounds=true
+        label4.layer.masksToBounds=true
+        
+        button1.layer.cornerRadius = 5
+        button2.layer.cornerRadius = 5
+        button3.layer.cornerRadius  = 5
+        button4.layer.cornerRadius = 5
+        
+        
+        
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ErasmusTU/PhoneViewController.swift
@@ -0,0 +1,123 @@
+//
+//  PhoneViewController.swift
+//  ErasmusTU
+//
+//  Created by Julia Martinez Ferrer on 01/06/16.
+//  Copyright © 2016 Michael Gänsluckner. All rights reserved.
+//
+
+import UIKit
+
+class PhoneViewController: UIViewController {
+
+    @IBOutlet var labelEsn: UILabel!
+    
+    @IBOutlet var label1: UIView!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label3: UILabel!
+    @IBOutlet var label4: UILabel!
+    @IBOutlet var label5: UILabel!
+    @IBOutlet var label6: UILabel!
+    @IBOutlet var label7: UILabel!
+    @IBOutlet var label8: UILabel!
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var  dataToTransfer: String = ""
+    
+    @IBAction func phoneInfo(sender: UIButton) {
+        dataToTransfer = "http://unigraz.esnaustria.org/mobile-phone-information"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)
+
+    }
+    
+    @IBAction func freeSim(sender: UIButton) {
+        dataToTransfer = "http://unigraz.esnaustria.org/free-sim-card"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)
+    }
+    @IBAction func comparator(sender: UIButton) {
+        dataToTransfer = "https://www.tarife.at/telefon-internet/handytarife"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)
+    }
+    
+    @IBAction func A1(sender: UIButton) {
+        dataToTransfer = "https://www.a1.net/"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)
+    }
+    @IBAction func Hot(sender: UIButton) {
+        dataToTransfer = "https://www.hot.at/"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)    }
+    
+    @IBAction func Tmobile(sender: UIButton) {
+        dataToTransfer = "http://www.t-mobile.at/"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)    }
+    
+    @IBAction func Drei(sender: UIButton) {
+        dataToTransfer = "https://www.drei.at/portal/de/privat/index.html"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)    }
+    
+    @IBAction func Telering(sender: UIButton) {
+        dataToTransfer = "http://www.telering.at/"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)    }
+    
+    @IBAction func Tele2(sender: UIButton) {
+        dataToTransfer = "https://www.tele2.at/"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)    }
+    
+    @IBAction func Yesss(sender: UIButton) {
+        dataToTransfer = "https://www.yesss.at/"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)    }
+    
+    @IBAction func Bob(sender: UIButton) {
+        dataToTransfer = "http://www.bob.at/"
+        self.performSegueWithIdentifier("PhoneToWeb", sender: self)    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label1.layer.cornerRadius = 5
+        label2.layer.cornerRadius = 5
+        label3.layer.cornerRadius = 5
+        label4.layer.cornerRadius = 5
+        label5.layer.cornerRadius = 5
+        label6.layer.cornerRadius = 5
+        label7.layer.cornerRadius = 5
+        label8.layer.cornerRadius = 5
+        
+        label1.layer.masksToBounds=true
+        label2.layer.masksToBounds=true
+        label3.layer.masksToBounds=true
+        label4.layer.masksToBounds=true
+        label5.layer.masksToBounds=true
+        label6.layer.masksToBounds=true
+        label7.layer.masksToBounds=true
+        label8.layer.masksToBounds=true
+        
+        button1.layer.cornerRadius = 5
+        button2.layer.cornerRadius = 5
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
